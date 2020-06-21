34d0b2b14dfe46576647423b164782f202b7b8f2
MahmoudYoussef97 <Mahmoudyoussef0097@gmail.com>
2020-06-21 02:43:26 +0200

    Fundmentals of Clean and Maintainable Testing

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{   
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        [Test]
+        [Ignore("Because I wanted to xD")]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_MaxWhenCalled_ReturnGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
